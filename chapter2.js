240e235711bebda6801d83667ba5ea05ae172b92
RachelNolan <mail@rachelnolan.com>
2015-08-12 11:32:08 +0100

    Completes challenge 3 of chapter 2

--- chapter2.js
+++ chapter2.js
@@ -40,3 +40,21 @@ for (var number = 1; number <= 100; number++) {
   console.log(printOut || n);
 };
 
+
+// Write a program that creates a string that represents an 8×8 grid, 
+// using newline characters to separate lines. At each position of the 
+// grid there is either a space or a “#” character. The characters 
+// should form a chess board.
+
+var gridSize = 8; grid = "";
+
+for (var line = 0; line < gridSize; line++) {
+  for (var block = 0; block < gridSize; block++) {
+    if ((line + block) % 2 == 0)
+      grid += " ";
+    else
+      grid += "#";
+  }
+  grid += "\n";
+};
+console.log(grid);
