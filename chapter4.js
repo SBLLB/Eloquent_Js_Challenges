fe2932166ded13e1354de2b0f30bb3c9940eb409
Rachel Nolan <rachel.nolan@atos.net>
2015-08-12 16:59:00 +0100

    Completes challenge 2 in chapter 4

--- chapter4.js
+++ chapter4.js
@@ -52,3 +52,48 @@ console.log(sum(rangeStep(1, 10)));
 // → 55
 console.log(rangeStep(5, 2, -1));
 // → [5, 4, 3, 2]
+
+
+// Write two functions, reverseArray and reverseArrayInPlace. 
+// The first, reverseArray, takes an array as argument and produces 
+// a new array that has the same elements in the inverse order. 
+// The second, reverseArrayInPlace, does what the reverse method 
+// does: it modifies the array given as argument in order to 
+// reverse its elements. Neither may use the standard reverse method.
+
+function reverseArray(array) {
+  var newArray = [];
+  for (var i = array.length -1; i >= 0; i--) {
+    newArray.push(array[i])
+  };
+  return newArray;
+};
+
+console.log(reverseArray(["A", "B", "C"]));
+
+
+function reverseArrayInPlace(array) {
+  var newArray = [];
+  for (var i = array.length -1; i >= 0; i--) {
+    newArray.push(array[i])
+  };
+  array = newArray;
+  return array;
+};
+
+var arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
+// → ["C", "B", "A"];
+
+// Textbook answer - 
+
+function reverseArrayInPlace(array) {
+  for (var i = 0; i < Math.floor(array.length / 2); i++) {
+    var old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
+}
