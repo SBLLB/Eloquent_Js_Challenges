dea6aada70daaa5a9fb4e971dae6d5f1bed857a5
RachelNolan <mail@rachelnolan.com>
2015-08-12 11:23:14 +0100

    Completes challenge 1 of Chapter 2

---
+++ chapter2.js
@@ -0,0 +1,14 @@
+// Write a loop that makes 7 calls to console.log to output the following
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+for (var line = "#"; line.length < 8; line += "#") {
+	console.log(line;)
+};
+
